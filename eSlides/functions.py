22a562278337b699601c7c60c38754472c2e817d
Daniel <daniel@enaitech.com>
2023-11-04 02:39:54 +0000

    ya quedo por hoy python Lab124.caesar-cipher.py python Lab124.caesar-cipher.py

---
+++ eSlides/functions.py
@@ -0,0 +1,38 @@
+print()
+print("---------------------------------------")
+print()
+def demo(x):
+    y = x + 3
+    return y
+
+print(demo(3))
+
+print()
+print("---------------------------------------")
+print()
+
+a = 1
+b = 2
+c = 3
+
+def sumando():
+    y = (a+b+c)
+    print(y)
+    
+sumando()
+print()
+print("---Function without Argument------------------------------------")
+print()
+
+def helloUser():
+    print("Hello User")
+helloUser()
+print()
+print("---Function with Argument------------------------------------")
+print()
+
+def hiUser(name):
+    print("Hello " + name)
+hiUser("Susan")
+
+
