1e252c7a5e3940a06eb174741df9d5023c175094
EC2 Default User <ec2-user@ip-10-16-10-83.us-west-2.compute.internal>
2023-11-04 01:09:54 +0000

    Daniel+ el 3 de noviembre al terminar conditionals

---
+++ eSlides/conditionals.py
@@ -0,0 +1,8 @@
+bananasCount = int(input("Dime cuantas bananas tienes (debe ser un numero del 0 al 10: "))
+
+if bananasCount >= 7:
+    print("You have a large bunch of bananas!!!")
+elif bananasCount >= 1:
+    print ("You have a small bunch of bananas")
+else:
+    print("Yoy don´t have bananas!!!")
